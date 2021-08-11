41b18e4767a9cd0c0637955de897b4d17ebdf846
Michael Curran <mcurran1722@gmail.com>
2021-08-11 13:19:02 -0600

    mobile style fix and log removal

--- pages/api/v1/txs/[txHash].js
+++ pages/api/v1/txs/[txHash].js
@@ -2,7 +2,6 @@ import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
 
 export default async function handler(req, res) {
 const { txHash } = req.query;
-console.log(txHash);
 const API = new BlockFrostAPI({
     projectId: process.env.BLOCKFROST_PROJECT_ID, // see: https://blockfrost.io
   });
